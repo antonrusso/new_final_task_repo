9cffb6bdfaa18999ed60d53e12f30d7e5f0fe56f
antonrusso <aantonrusso@gmail.com>
2020-10-22 22:11:08 +0300

    upd classes

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,11 @@
-link = "http://selenium1py.pythonanywhere.com/"
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
 
 def go_to_login_page(browser):
