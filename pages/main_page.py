bdf628892bfc05095bc2a0fe0fd50c75f77d3e89
antonrusso <aantonrusso@gmail.com>
2020-10-23 19:10:16 +0300

    preparing for step 4.3

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,7 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
-from .login_page import LoginPage
+from selenium.common.exceptions import NoAlertPresentException
+import math
 
 
 class MainPage(BasePage):
@@ -11,3 +12,17 @@ class MainPage(BasePage):
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
